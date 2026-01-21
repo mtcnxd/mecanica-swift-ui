62437b0e6627b09e1d1b1afe369b99712231cb4b
Marcos Tzuc Cén <mtc.nxd@gmail.com>
2026-01-21 07:27:42 -0600

    Initial Commit

---
+++ mecanicaRubio/mecanicaRubioApp.swift
@@ -0,0 +1,17 @@
+//
+//  mecanicaRubioApp.swift
+//  mecanicaRubio
+//
+//  Created by mtcnxd on 21/01/26.
+//
+
+import SwiftUI
+
+@main
+struct mecanicaRubioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

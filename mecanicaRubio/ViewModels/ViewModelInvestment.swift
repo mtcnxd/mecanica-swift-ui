f544d5352bb214e6e2bd6e7486e2e4500db6787c
Marcos Tzuc Cén <mtc.nxd@gmail.com>
2026-05-04 21:43:42 -0600

    Actualizar ViewModelInvestment.swift

--- mecanicaRubio/ViewModels/ViewModelInvestment.swift
+++ mecanicaRubio/ViewModels/ViewModelInvestment.swift
@@ -6,25 +6,25 @@ class ViewModelInvestment : ObservableObject {
     @Published var investments = [Investment]()
     @Published var investmentsItems = [InvestmentItem]()
     
-    init(){
+    private let repository : Repository
+    
+    init(repository: Repository = Repository()){
+        self.repository = repository
         print("Start class View Model Investment")
-        self.getCryptoInvestments()
     }
     
-    func getInvestments(){
-        Task {
-            let response = try await Repository().getInvestments()
+    func getInvestments() async {
+        do {
+            let response = try await repository.getInvestments()
             self.investments = response.data
+        } catch {
+            errorMessage = error.localizedDescription
+            print("Error")
         }
     }
     
-    func getCryptoInvestments(){
-        Task {
-            let response = try await Repository().getCryptoInvestments()
-            
-            print(response)
-            
-            self.investmentsItems = response.items
-        }
+    func getCryptoInvestments() async {
+        let response = try await repository.getCryptoInvestments()
+        self.investmentsItems = response.items
     }
 }
